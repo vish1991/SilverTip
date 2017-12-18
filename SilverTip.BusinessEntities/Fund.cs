4a4c35f35a7aecc062fbf214255b32a22fe11fb2
LahiruT94 <gayangaextreme@gmail.com>
2017-12-18 15:38:56 +0530

    Adds tables

---
+++ SilverTip.BusinessEntities/Fund.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Boughtleaf.BusinessEntities
+{
+    public class Funds
+    {
+        [Key]
+        public int Id { get; set; }
+        public String Name { get; set; }
+
+        [MaxLength(500)]
+        public String Description { get; set; }
+        public Boolean IsActive { get; set; }
+        public String Code { get; set; }
+        public int AccountNumber { get; set; }
+        public String Branch { get; set; }
+        [ForeignKey("BankId")]
+        public Banks Bank { get; set; }
+    }
+}
---
+++ SilverTip.BusinessEntities/ItemCategory.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Boughtleaf.BusinessEntities
+{
+    public class ItemCategories
+    {
+        [Key]
+        public int Id { get; set; }
+        public String Code { get; set; }
+
+        public string Name { get; set; }
+    }
+}
